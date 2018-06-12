108495efba7e2710f284a771d1c1f8e5db7d5495
champ <thechamp2535@gmail.com>
2018-06-12 14:28:54 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage',{
-        from: "Champ",
-        text: "Yup, that work for me"
-    })
 });
 
 socket.on('disconnect', function() {
